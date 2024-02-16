017f14950c0b7f78ee0c78774032d64f04ea54e9
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-02-16 12:19:01 -0800

    Added comments to help plan out how the program will work and function in main.py

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ from userdata import userChoices
 # Program start Message
 print('Lets Play Rock, Paper, Scissors')
 input('Press a Key to Continue...')
+
+# Algorithm will pick a random number that will determine which out of the 3 choices it will chose
+# After the Algorithm sufficent data on what the user has previously chosen, it will begin to predict the users choice based on the data
