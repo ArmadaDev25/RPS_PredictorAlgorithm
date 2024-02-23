fa65bebe36854ea495748273c42dd0168e80e25d
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-02-23 07:55:04 -0800

    Started work on the main program loop

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import random
 
 # Variables
 userChoices = [] # Holds the User's previous choices
+logicRun = True
+userEntry = None
+algorEntry = None
 
 # Program start Message
 print('Lets Play Rock, Paper, Scissors')
@@ -15,4 +18,10 @@ input('Press a Key to Continue...')
 # Algorithm will pick a random number that will determine which out of the 3 choices it will chose
 # After the Algorithm sufficent data on what the user has previously chosen, it will begin to predict the users choice based on the data
 
-deterRes("hello", "me")
+
+# Main Program Loop
+while logicRun:
+    userEntry = input("Rock, Paper, or Scissors?")
+    
+    deterRes("hello", "me") # Debug
+    logicRun = False 
